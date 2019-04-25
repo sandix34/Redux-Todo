dd4291e44d713a7c7dbf47e529b247fd1731ea95
sandrine <sandrinezocca@gmail.com>
2019-04-25 14:10:51 +0200

    reducer divide

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,8 +2,8 @@ import * as actions from './actions';
 
 // nous avons un state de cette forme
 //  {
-//    todos: [],
-//    filter: ''
+//    todos: [], ---------> todoReducer
+//    filter: '' ---------> filterReducer
 //  }
 // todos contient un tableau de todo qui ont cette forme
 //  {
@@ -11,40 +11,51 @@ import * as actions from './actions';
 //    done: true
 //  }
 
-export const todosReducer = (state, action) => {
+const todoReducer = (state, action) => {
     switch(action.type) {
         case actions.ADD_TODO : {
             return {
-                ...state,// copie toutes les propriétés du state courant avec l'opérateur spread
                 todos: [ ...state.todos, action.todo ]//copie tous les éléments du state.todos et ajout todo
             }
         }
         case actions.DELETE_TODO : {
             return {
-                ...state,
-                todos: state.todos.filter( (t, i) => i !== action.index )
-            }
-        }
-        case actions.SET_FILTER : {
-            return {
-                ...state,
-                filter: action.filter
+               state.filter( (t, i) => i !== action.index )
             }
         }
         case actions.TOGGLE_TODO : {
-            return {
-                ...state,
-                todos: state.todos.map( (t, i) => {
+            return state.map( (t, i) => {
                     if (i === action.index) {
                         t.done = !t.done
                     }
-                    return t;
+                    return t
                 })
-            }
         }
         // si aucune action n'est identifiée on retourne par défaut le state courant
         default: {
             return state
         }
     }
+}
+
+const filterReducer = (state, action) => {
+    switch(action.type) {
+        case actions.SET_FILTER : {
+            return action.filter
+            
+        }
+       // si aucune action n'est identifiée on retourne par défaut le state courant
+       default: {
+        return state
+    } 
+    }
+}
+
+// c'est uniquement todosReducer qui sera récupéré par le store et il comprend l'intégralité des différents effets
+// des différentes actions
+export const todosReducer = (state, action) => {
+   return {
+       todos: todoReducer(state.todos, action),
+       filter: filterReducer(state.filter, action)
+   }
 }
