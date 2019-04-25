c7666683c6d50b8038295f0a3decbb9698bccd00
sandrine <sandrinezocca@gmail.com>
2019-04-25 14:35:05 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { todosReducer } from './store/reducers';
+
+const store = createStore(todosReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
