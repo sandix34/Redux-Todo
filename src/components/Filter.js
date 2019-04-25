e930f25e7d19f43cb352fa2e6f3df060a0543036
sandrine <sandrinezocca@gmail.com>
2019-04-25 16:15:32 +0200

    connect() & mapDispatchToProps

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {setFilter, visibilityFilters } from '../store/actions';
 
-const Filter = ({ dispatch }) => {
+const Filter = ({ setFilter }) => {
     return (
        <> 
         <button 
         className="btn btn-primary mr-2" 
-        onClick={ () => dispatch(setFilter(visibilityFilters.SHOW_ALL)) } 
+        onClick={ () => setFilter(visibilityFilters.SHOW_ALL) } 
         >Tout</button>
         <button  
         className="btn btn-primary mr-2"
-        onClick={ () => dispatch(setFilter(visibilityFilters.SHOW_DONE)) }
+        onClick={ () => setFilter(visibilityFilters.SHOW_DONE) }
         >Fini</button>
         <button 
         className="btn btn-primary"
-        onClick={ () => dispatch(setFilter(visibilityFilters.SHOW_ACTIVE)) }
+        onClick={ () => setFilter(visibilityFilters.SHOW_ACTIVE) }
         >En cours</button>
        </> 
     )
 }
 
-export default connect()(Filter); 
+export default connect(null, {
+    setFilter
+})(Filter); 
